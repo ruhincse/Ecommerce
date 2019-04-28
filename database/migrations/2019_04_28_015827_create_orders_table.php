cebdbc662f9efdb6306ee69768a92d7c57b66554
ruhincse <ruhinm3@gmail.com>
2019-04-28 08:25:27 +0600

    Database Schema Design

---
+++ database/migrations/2019_04_28_015827_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('customer_name');
+            $table->text('customer_address');
+            $table->integer('customer_mobile');
+            $table->decimal('total_amount',11);
+            $table->string('payment_status')->default('pending');
+            $table->text('payment_details')->nullable();
+            $table->string('order_status')->default('pending');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
